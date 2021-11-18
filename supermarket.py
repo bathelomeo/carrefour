7f00d8b947bf21626dc766fe571efb5dc8f6e777
bathelomeo <ngethenathaniel@gmail.com>
2021-11-18 19:29:17 +0400

    Update

--- supermarket.py
+++ supermarket.py
@@ -1,83 +1,51 @@
-print('Welcome to Carrefour')
-Give = input('You can only buy the things in this categories, Agree?:')
-if Give == 'Ok':
-    print('Lets Continue')
-Question = input('Do you want to buy something in Vegetables , Fruits , Snacks , Bisquits and only that.')
-if Question == 'Yes':
-    print('Great')
-elif Question == 'No':
-    print('Sorry i cant help you')
-price = 0
-Vegetables =input('Do you want to know what is in Vegetables')
+print('hi')
+greeting = input('What time of day is it ')
+if greeting =='Afternoon':
+    print('Good Afternoon')
 
-if Vegetables == 'Yes':
-    print('In Vegetables There is: Tomatoes , Onion , Brocoli , Cauliflower')
+if greeting == 'Evening':
+    print('Good Evening')
+
+if greeting== 'Morning':
+    print('Good morning')
+Name = input('May i ask what is you name')
+print('Hello' ' '+ Name)
+
+print('lets go with a security check')
+
+security = input('hey other guard can you see any guns or bombs')
+
+if security== 'Yes':
+    print('Put your hands in the Air')
 else:
-    print('Fine')
-vegies = input ('So what do you want to buy in Vegetables')
+    print('go through')
 
-if vegies == 'Tomatoes':
-    price = 0.7
-elif vegies == 'Onion':
-    price = 0.5
-elif vegies == 'Brocoli':
-    price = 0.25
-elif vegies == 'Cauliflower':
-    price = 0.30
-print(price)
-
-Fruits =input('Do you want to know what is in fruits')
-
-if Fruits == 'Yes':
-    print('In Fruits There is: Grapes , Orange , Banana , Kiwi')
+corona = input('May we please check your temprature and please sanitize')
+if corona== 'Yes':
+    print('You may go in Madam or Mr')
 else:
-    print('Fine')
-Frouts = input ('So what do you want to buy in Fruits')
+    print('Sorry we cant let you in')
 
-if Frouts == 'Grapes':
-    price = 0.300
-elif Frouts == 'Orange':
-    price = 0.30
-elif Frouts == 'Banana':
-    price = 0.10
-elif Frouts == 'Kiwi':
-    price = 0.100
-print(price)
-
-Snacks =input('Do you want to know what is in Snack')
-
-if Snacks == 'Yes':
-    print('In Fruits There is: Stix , Kra curls , Tamu Tamu , Crakles ')
-elif Snacks == 'No':
-    print('Fine')
-Snocks = input ('So what do you want to buy in Snacks')
-
-if Snocks == 'Stix':
-    price = 0.30
-elif Snocks == 'Kra curls':
-    price = 0.45
-elif Snocks == 'Tamu Tamu':
-    price = 0.20
-elif Snocks == 'Kracles':
-    price = 0.50
-print(price)
-
-Bisquits =input('Do you want to know what is in Bisquits')
-
-if Bisquits == 'Yes':
-    print('In Fruits There is: Oreo , Mary land , Nice , Wafers')
-elif Bisquits == 'No':
-    print('Fine')
-
-Bosquits = input ('So what do you want to buy in Bisquits')
-
-if Bosquits == 'Oreo':
-    price = 0.60
-elif Bosquits == 'Mary land':
-    price = 0.150
-elif Bosquits== 'Nice':
-    price = 0.56
-elif Bosquits == 'Wafers':
-    price = 0.70
-print(price)
-print('Everything that is one number or is mary land cookies price is actually a 0 in there')
+Troley = input('Here is your Troley')
+if Troley== 'Thank you':
+    print('Your Welcome')
+else:
+    print('Ahhhhh Ok')
+pay = input('What Will you be paying with')
+if pay=='Mpesa':
+    print('Ahhh Ok')
+elif pay=='safaricom':
+    print('Ahhhh Ok')
+elif pay=='cash':
+    print('Ahhhh Ok')
+else:
+    print('Mrs or Mr sorry but you will have to return all this things')
+parking = input('What Will you be paying with')
+if parking=='Mpesa':
+    print('Ahhh Ok')
+elif parking=='Safaricom':
+    print('Ahhhh Ok')
+elif parking=='Cash':
+    print('Ahhhh Ok')
+print('Please insert your card')
+print('You have 30 minutes to exit the premisise')
